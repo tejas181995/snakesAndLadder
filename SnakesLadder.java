08c4803a3b47dc7f6a5f586716703bfb503aa3a9
tejas181995 <tejasdev92@gmail.com>
2021-02-22 22:07:24 +0530

    updated with values of ladder and snake

--- SnakesLadder.java
+++ SnakesLadder.java
@@ -1,11 +1,23 @@
 public class SnakesLadder {
+    public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
     public static void main(String[] args) {
         System.out.println("Welcome to Snakes and ladder program");
         int player1_Position = 0;
-        System.out.println("Player 1 start with position: " + player1_Position);
-        double die = Math.floor(Math.random() * 10) % 6 + 1;
-        player1_Position += die;
-        System.out.println("Player 1 start with position: " + player1_Position);
+        int option = (int) Math.floor(Math.random() * 10) % 3;
+        int die = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        if(option == NO_PLAY){
+            System.out.println("no need to play and position is: " + player1_Position );
+        }else if(option == LADDER){
+            player1_Position += die;
+            System.out.println("Position of player is: " + player1_Position);
+        }else{
+            player1_Position -= die;
+            player1_Position = player1_Position < 0 ? 0 : player1_Position;
+            System.out.println("Position of player is: " + player1_Position);
+        }
+
+        
     }
 }
-
