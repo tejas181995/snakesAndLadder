ce3d757e794a63c7277a53bfefe3e7f699b2c619
tejas181995 <tejasdev92@gmail.com>
2021-02-22 22:23:20 +0530

    updated with winning condition

--- SnakesLadder.java
+++ SnakesLadder.java
@@ -5,19 +5,24 @@ public class SnakesLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snakes and ladder program");
         int player1_Position = 0;
-        int option = (int) Math.floor(Math.random() * 10) % 3;
-        int die = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        if(option == NO_PLAY){
-            System.out.println("no need to play and position is: " + player1_Position );
-        }else if(option == LADDER){
-            player1_Position += die;
-            System.out.println("Position of player is: " + player1_Position);
-        }else{
-            player1_Position -= die;
-            player1_Position = player1_Position < 0 ? 0 : player1_Position;
-            System.out.println("Position of player is: " + player1_Position);
-        }
+        while(player1_Position != 100){
+            int option = (int) Math.floor(Math.random() * 10) % 3;
+            int die = (int) Math.floor(Math.random() * 10) % 6 + 1;
+            if(option == NO_PLAY){
+                System.out.println("no need to play and position is: " + player1_Position );
+            }else if(option == LADDER){
+                player1_Position += die;
+                player1_Position = player1_Position > 100 ? player1_Position - die : player1_Position;
+                System.out.println("Position of player is: " + player1_Position);
+            }else{
+                player1_Position -= die;
+                player1_Position = player1_Position < 0 ? 0 : player1_Position;
+                System.out.println("Position of player is: " + player1_Position);
+            }
 
+        }
+        System.out.println("player 1 won !!!");
+       
         
     }
 }
